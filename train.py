116f575fb5637efa511ee2750f5e830a7794def8
kovaleski100 <mfkovaleski@inf.ufrgs.br>
2021-05-09 17:42:38 -0300

    teste epoch == 1000

--- train.py
+++ train.py
@@ -99,7 +99,7 @@ for epoch in range(100):  # loop over the dataset multiple times
 
         # print statistics
         running_loss += loss.item()
-        if i % 50 == 19:    # print every 2000 mini-batches
+        if i % 500 == 199:    # print every 2000 mini-batches
             print('[%d, %5d] loss: %.3f' %
                   (epoch + 1, i + 1, running_loss / 50))
             running_loss = 0.0
